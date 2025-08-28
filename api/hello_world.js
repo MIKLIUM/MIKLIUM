0a7e6ab4d3f85ef9d4e8b80049d39bb179d4eecc
MIKL <coatingshightech64@gmail.com>
2025-08-28 20:47:16 +0300

    Update hello_world.js

--- api/hello_world.js
+++ api/hello_world.js
@@ -4,7 +4,7 @@ module.exports = async function handler(req, res) {
         res.status(200).json({
           hello: "world"
         });
-    } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+    } 
+  } catch (error) {
+    res.status(500).json({ error: error.message });
 };
