6246b0a127aaaff12177300a5127c535e109f609
MIKL <coatingshightech64@gmail.com>
2025-12-13 18:06:49 +0300

    Update 404.js

--- api/404.js
+++ api/404.js
@@ -1,4 +1,4 @@
 async function handler(req, res) {
-  res.status(404).json({ success: false, error: "Not Found" });
+  res.status(404).json({ success: false, error: "Not Found. List and documentation of available MIKLIUM APIs: https://github.com/MIKLIUM/MIKLIUM/blob/main/APIDOCS.md" });
 };
 module.exports = handler;
