c69996b11eff9b632ba1b0e41cccb102ac038c65
MIKL <219871313+mikl-shortcuts@users.noreply.github.com>
2026-03-02 20:16:19 +0300

    Update index.js

--- api/shortcut-info/index.js
+++ api/shortcut-info/index.js
@@ -1,6 +1,15 @@
 const fetch = require('node-fetch');
 
 async function handler(req, res) {
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   if (req.method !== 'GET' && req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
