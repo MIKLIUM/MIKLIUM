417adce8f2abad104113ddd61bf710f004aa3b14
MIKL <219871313+mikl-shortcuts@users.noreply.github.com>
2026-03-02 20:14:11 +0300

    Update index.js

--- api/youtube-transcript/index.js
+++ api/youtube-transcript/index.js
@@ -5,6 +5,15 @@ const client = new ApifyClient({
 });
 
 async function handler(req, res) {
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   try {
     let url, removeTimestamps, includeInfo;
     
